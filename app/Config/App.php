0e053e7042316b0153a3a4de568fca748f22e076
jayasree <jayasree916@gmail.com>
2025-03-09 13:12:33 +0530

    Updated baseURL from .env file

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,15 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Dynamically set the baseURL from the .env file
+        $this->baseURL = getenv('app.baseURL');
+    }
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
